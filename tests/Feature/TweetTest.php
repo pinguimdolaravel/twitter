e365ca7b2774234744481c777d3631b36a6c1690
Rafael Lunardelli <rafael@devsquad.com>
2023-03-30 16:23:40 -0600

    planning tweet feature

---
+++ tests/Feature/TweetTest.php
@@ -0,0 +1,6 @@
+<?php
+
+todo('should be able to create a tweet');
+todo('body is required');
+todo('the tweet body should have a max length of 140 characters');
+todo('should show the tweet on the timeline');
